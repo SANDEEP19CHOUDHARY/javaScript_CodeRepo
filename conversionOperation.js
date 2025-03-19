083fedb74c70d0f66665601682b9b1eb9f66b10b
SANDEEP19CHOUDHARY <156289605+SANDEEP19CHOUDHARY@users.noreply.github.com>
2025-03-19 18:53:57 +0000

    conversionOperation

---
+++ conversionOperation.js
@@ -0,0 +1,20 @@
+let score="bb63"
+//console.log(typeof(score))
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//Number "33"->33
+//"aa33"->NaN
+//True->1, false->0
+
+//let isLoggedin=1;
+let isLoggedin="Sandeep"
+let booleanIsLoggedIn=Boolean(isLoggedin)
+console.log(booleanIsLoggedIn)
+
+//1->true; 0->false
+//""->false
+//"sandeep"-> true
+
