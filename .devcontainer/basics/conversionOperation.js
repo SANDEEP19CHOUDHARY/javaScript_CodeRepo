4f157940534863cd9ba1e589dcc6a18a61fbc836
SANDEEP19CHOUDHARY <156289605+SANDEEP19CHOUDHARY@users.noreply.github.com>
2025-03-19 19:40:21 +0000

    Few added lines

--- .devcontainer/basics/conversionOperation.js
+++ .devcontainer/basics/conversionOperation.js
@@ -12,9 +12,34 @@ let valueInNumber=Number(score)
 //let isLoggedin=1;
 let isLoggedin="Sandeep"
 let booleanIsLoggedIn=Boolean(isLoggedin)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 //1->true; 0->false
 //""->false
 //"sandeep"-> true
 
+let value=3
+let negValue=-value
+//console.log(negValue)
+
+let str1="hello"
+let str2="Sandeep"
+let str3= str1+str2
+//console.log(str3)
+/*console.log("2"+1+2)
+console.log(1+2+"3")
+console.log(1+2*5%3)*/
+
+
+let num1, num2, num3
+num1=num2=num3=4
+
+let gameCounter=100;
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter)
+// Link to read -> https://262.ecma-international.org/14.0/index.html?_gl=1*rx8lza*_ga*NjM3NjU2ODMuMTc0MjQwNzc1Mw..*_ga_TDCK4DWEPP*MTc0MjQxMTgwOC4yLjAuMTc0MjQxMTgxNS4wLjAuMA..
+// Comparision and equality check is different
+
+//===(strict check)
+console.log("2"===2)
