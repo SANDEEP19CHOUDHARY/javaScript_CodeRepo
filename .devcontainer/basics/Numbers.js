4cafdd7ad77df2cdee89e1b061840d22602b5a7c
SANDEEP19CHOUDHARY <156289605+SANDEEP19CHOUDHARY@users.noreply.github.com>
2025-03-22 06:44:13 +0000

    Numbers

---
+++ .devcontainer/basics/Numbers.js
@@ -0,0 +1,13 @@
+const score= 40009090.784782;
+const balance= new Number(100);
+/* console.log(balance);
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+console.log(score.toPrecision(1))
+console.log(score.toLocaleString()) */
+
+/*************Maths******************* */
+
+const max=20;
+const min=10;
+console.log(Math.floor(Math.random()*(max-min+1)+min));
