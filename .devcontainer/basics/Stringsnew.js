b98adbe84bc88d5bf68ca297cf0378dceb71b81b
SANDEEP19CHOUDHARY <156289605+SANDEEP19CHOUDHARY@users.noreply.github.com>
2025-03-22 06:17:24 +0000

    Strings latest

---
+++ .devcontainer/basics/Stringsnew.js
@@ -0,0 +1,20 @@
+/*const name="Sandeep Choudhary"
+const repoCount= 50
+console.log(name+repoCount+" value")
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);*/
+
+const gameName=new String('Assasins Creed-Shadows');
+/*console.log(gameName)
+console.log(gameName[2])
+console.log(gameName.__proto__);
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt('6'))
+console.log(gameName.indexOf('s'))*/
+const latest= gameName.substring(0,4);
+const anotherSlice=gameName.slice(0,20)
+console.log(latest)
+console.log(anotherSlice)
+const newString= "  Misty  "
+console.log(newString.trim())
+//console.log(newString)
